5661275dcac0e038a66f6b85adf85a11b25de5bf
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2025-06-16 16:50:54 +0200

    Bump LCG version

--- setupROOTWithLCG.sh
+++ setupROOTWithLCG.sh
@@ -7,6 +7,13 @@ fi
 ########################################################################
 ### Note: Setup the following LCG stack.
 ########################################################################
-echo "Setup LCG_105a stack built for x86_64-el9-gcc12-opt which has gcc12 and ROOT 6.30.04"
-source /cvmfs/sft.cern.ch/lcg/views/LCG_105a/x86_64-el9-gcc12-opt/setup.sh
+if [[ !  ${CPLUS_INCLUDE_PATH} =~ LCG_106c/x86_64-el9-gcc13-opt ]]; then
+  echo "Setup LCG_106c stack built for x86_64-el9-gcc13-opt which has gcc13 and ROOT 6.32.10"
+  source /cvmfs/sft.cern.ch/lcg/views/LCG_106c/x86_64-el9-gcc13-opt/setup.sh
+fi
 
+
+###if [[ !  ${CPLUS_INCLUDE_PATH} =~ LCG_105c/x86_64-el9-gcc12-opt ]]; then
+###  echo "Setup LCG_105c stack built for x86_64-el9-gcc12-opt which has gcc12 and ROOT 6.30.08"
+###  source /cvmfs/sft.cern.ch/lcg/views/LCG_105c/x86_64-el9-gcc12-opt/setup.sh
+###fi
